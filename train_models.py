cad2b99d4489bd8385ce4da66b524662cf126ce8
Sagesse PC <sagesseariyanto@gmail.com>
2025-12-28 21:12:15 -0700

    completed category model predictor

--- train_models.py
+++ train_models.py
@@ -1,22 +1,31 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 # Load dataset
 data = pd.read_csv("./ai_data.csv")
 
 # TODO: FUNCTION TO DETERMINE CATEGORY BASED ON DESCRIPTION
-data_categories = data.dropna(subset=["Description", "Category"])       # drop rows with missing Description or Category
+data_categories = data.dropna(
+    subset=["Description", "Category"]
+)  # drop rows with missing Description or Category
 
 # Split data into features X and target y (y = f(X))
-X_desc = data_categories['Description']                                 # feature column
-y_category = data_categories["Category"]                                # target column
+X_desc = data_categories["Description"]  # feature column
+y_category = data_categories["Category"]  # target column
 
 # split data into training (80%) and testing (20%) sets
-X_train, X_test, y_train, y_test = train_test_split(X_desc, y_category, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_desc, y_category, test_size=0.2, random_state=42
+)
 
 # ML models learn better with numerical data
-vectorizer = TfidfVectorizer(max_features=100, stop_words='english')                # limit to top 100 features with common English stop words removed
+vectorizer = TfidfVectorizer(
+    max_features=1000, stop_words="english"
+)  # limit to top 1000 features with common English stop words removed
 
 # Fit training data and transform it (learn + convert)
 X_train_vec = vectorizer.fit_transform(X_train)
@@ -24,7 +33,14 @@ X_train_vec = vectorizer.fit_transform(X_train)
 # Transform testing data (convert only)
 X_test_vec = vectorizer.transform(X_test)
 
+# Train the model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train_vec, y_train)
+y_prediction = model.predict(X_test_vec)  # final y prediction values from the model
+accuracy = accuracy_score(
+    y_test, y_prediction
+)  # accuracy of the model compared to actual y test values
 
-
-
-
+# Save the trained model and vectorizer (wb = write binary)
+pickle.dump(model, open("./Models/model_category.pkl", "wb"))
+pickle.dump(vectorizer, open("./Models/vectorizer_category.pkl", "wb"))
