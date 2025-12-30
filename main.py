7060e5e9e125bf31a80bc0b4da1bca7f96b8093b
Sagesse PC <sagesseariyanto@gmail.com>
2025-12-30 15:40:34 -0700

    loaded models for stramlit

--- main.py
+++ main.py
@@ -12,12 +12,25 @@ def predict_category(desciption):
     category_prediction = category_model.predict(desc_vec)[0]
     return category_prediction
 
+def predict_success(description, category, price):
+    success_model = pickle.load(open("./Models/model_success.pkl", "rb"))
+    description_vectorizer = pickle.load(
+        open("./Models/vectorizer_description.pkl", "rb")
+    )
+    category_encoder = pickle.load(
+        open("./Models/label_encoder_category.pkl", "rb")
+    )
+    price_encoder = pickle.load(
+        open("./Models/label_encoder_price.pkl", "rb")
+    )
+    
+
 st.title("AI Predictor")
 
 company_name = st.text_input("Enter your AI company name:")
 description = st.text_area("Enter a brief description of your AI company:")
 
-if st.button("Predict Category"):
+if st.button("Predict"):
     if description:
         category = predict_category(description)
         st.success(f"The predicted category for your AI company is: {category}")
